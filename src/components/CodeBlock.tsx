2760fa1767ecb08b67544222ce250643ff2734a0
Umer <muhammadumershabir@gmail.com>
2023-03-19 17:30:22 +0500

    building up the blogs page

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 interface CodeBlockProps {
   node?: any;
   inline?: boolean;
