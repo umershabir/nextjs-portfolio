f77bb6e7cda0fc0d62c7ec0643c021a03bddce29
Umer <muhammadumershabir@gmail.com>
2023-04-04 00:03:28 +0500

    updated

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -7,7 +7,7 @@ import CodeBlock from "@/components/CodeBlock";
 import Image from "next/image";
 import styles from "../../styles/Blogs.module.scss";
 import Link from "next/link";
-import authorImage from "/public/author.jpg";
+import authorImage from "public/author.jpg";
 // post page
 export default function PostPage(props: any) {
   console.log(authorImage);
@@ -24,7 +24,7 @@ export default function PostPage(props: any) {
           <h2>{props.frontmatter.title}</h2>
           <div className={styles.author}>
             <img
-              src={authorImage.src}
+              src={"/" + authorImage.src}
               alt="author-image"
               style={{ borderRadius: 50, width: 50, height: 50 }}
             />
