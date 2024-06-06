8f50352e904c67aa6c199c8d792ab674a3f129bf
umer <muhammadumershabir@gmail.com>
2024-06-06 22:11:14 +0500

    changes in home page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,10 +2,13 @@ import styles from "./footer.module.scss";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href=" " className="secondaryText">
+      <a href="mail:muhammadumershabir@gmail.com" className="secondaryText">
         muhammadumershabir@gmail.com
       </a>
-      <p className="secondaryText">Theme by Umer Shabir</p>
+      <a href="https://wa.me/+923093961696" className="secondaryText">
+        wa +923093961696
+      </a>
+      {/* <p className="secondaryText">Theme by Umer Shabir</p> */}
     </footer>
   );
 }
